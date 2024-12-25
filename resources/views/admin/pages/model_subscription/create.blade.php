9bcb343aa2d52529b0d31a166048508f84569836
Jyl <lenoirkevin312@gmail.com>
2024-12-25 12:43:38 +0000

    1243

--- resources/views/admin/pages/model_subscription/create.blade.php
+++ resources/views/admin/pages/model_subscription/create.blade.php
@@ -137,11 +137,10 @@
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="alertModalLabel">Alerte</h5>
+                    <h5 class="modal-title" id="alertModalLabel">Message</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body" id="alertModalBody">
-                    <!-- Message d'alerte ici -->
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><i class="bi bi-x-circle"></i> Fermer</button>
